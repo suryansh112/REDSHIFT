3860705177925fccd7f680d2f8ff948276dd3e1d
Suryansh Srivastava <ss.srivastava.99@gmail.com>
2024-08-16 00:17:58 +0530

    fix

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -22,9 +22,9 @@ resource "aws_lambda_function" "my_lambda" {
   function_name = "pause-redshift"
   role          = aws_iam_role.iam_for_lambda.arn
   
-
+handler       = "lambda.handler"
   runtime = "python3.8"
-  filename      = "../lambda_function.zip"
+  filename      = "../lambda.zip"
 
 
 
