a5471abb90a80ea11123984b40db70fc3bf1ebdb
Suryansh Srivastava <ss.srivastava.99@gmail.com>
2024-08-18 00:11:53 +0530

    fix

--- modules.tf
+++ modules.tf
@@ -1,6 +1,11 @@
 module "redshift"{
     source = "./redshift"
     role = var.role
+     cluster_name = var.cluster_name
+     number_of_nodes =  var.number_of_nodes
+     cluster_type = var.cluster_type
+     node_type = var.node_type
+     database_name = var.database_name
 }
 
 module "lambda" {
