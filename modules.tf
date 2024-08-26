30896d679fcda9bca239a6e5d41f4c730e72b86e
Suryansh Srivastava <ss.srivastava.99@gmail.com>
2024-08-26 22:36:26 +0530

    fix

--- modules.tf
+++ modules.tf
@@ -14,4 +14,5 @@ module "lambda" {
     github_workspace = var.github_workspace
     redshift_lambda_access = var.redshift_lambda_access
     env_function_name = var.env_function_name
+    database_name = var.database_name
 }
